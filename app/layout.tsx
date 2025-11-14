5ee65843a24cd64665b049914f722752243b0733
kushugo <51033832+kushugo@users.noreply.github.com>
2025-11-14 10:41:39 +0100

    meta

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  )
+}
+
